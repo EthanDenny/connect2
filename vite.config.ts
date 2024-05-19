b21ae6a70b503bdd48c5697fbc922344c05409ef
Gavin Power <gavinmaxpower@gmail.com>
2024-05-19 13:48:54 -0230

    hi

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+      allowedHeaders: ["X-Requested-With", "content-type", "Authorization"],
+    },
+  },
+});
